1daad72aee570b1f3cc80676d6828014d47f7107
Ulises Ros <ulisesros70@gmail.com>
2023-07-04 14:59:18 -0300

    Primer commit

---
+++ src/ProductManager.js
@@ -0,0 +1,93 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+
+class ProductManager {
+
+    constructor(filename){
+        this.filename = filename
+        this.filepath = path.join(__dirname, this.filename)
+    }
+
+    async addProduct (producto) {
+        const data = await fs.readFile(this.filepath, 'utf-8')
+        const product = JSON.parse(data)
+
+        const newId = product[product.length - 1]?.id || 0
+        
+        
+        if(!producto.title || !producto.description || !producto.price || !producto.thumbnail || !producto.code || !producto.stock){
+            console.log('Todos los campos son obligatorios')
+        } else if (product.find(prod => prod.title == producto.title || prod.code == producto.code)) {
+            console.log('No se puede repetir el title o code de un producto') 
+        } else {
+            console.log(`El Producto "${producto.title}" fue creado con exito`)
+
+            product.push({
+                ...producto,
+                id: newId + 1
+            })
+        }
+        
+        await fs.writeFile(this.filepath, JSON.stringify(product, null, 2))
+    }
+
+    async getProducts () {
+        const data = await fs.readFile(this.filepath, 'utf-8')
+        const product = JSON.parse(data)
+
+        return product
+    }
+
+    async getProductById (id) {
+        const data = await fs.readFile(this.filepath, 'utf-8')
+        const product = JSON.parse(data)
+        
+        const productId = product.find(prod => prod.id == id)
+
+        return !productId ? "ID INEXISTENTE" : productId
+    }
+    
+    async updateProduct (id, edit) {
+        const data = await fs.readFile(this.filepath, 'utf-8')
+        const product = JSON.parse(data)
+
+        let productId = product.find(prod => prod.id == id)
+        const productDelete = product.filter(prod => prod.id !== id)
+        
+        const productEdit = {
+            ...productId,
+            ...edit
+        }
+
+        if(productId){
+            await fs.writeFile(this.filepath, JSON.stringify(productDelete, null, 2))
+            productDelete.push(productEdit)
+        } else{
+            console.log("ID INEXISTENTE")
+        }
+
+        await fs.writeFile(this.filepath, JSON.stringify(productDelete, null, 2))
+    }
+
+    async deleteProduct (id) {
+        const data = await fs.readFile(this.filepath, 'utf-8')
+        const product = JSON.parse(data)
+
+        const productId = product.find(prod => prod.id == id)
+
+        if(productId){
+            const productDelete = product.filter(prod => prod.id !== id)
+            await fs.writeFile(this.filepath, JSON.stringify(productDelete, null, 2))
+            console.log(`El producto con id: ${productId.id} ha sido eliminado`)
+        }
+        else {
+            console.log("ID INEXISTENTE")
+        }
+    }
+
+}
+
+module.exports = ProductManager
+
+
---
+++ src/app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const ProductManager = require('./ProductManager')
+
+const app = express()
+
+const productManager = new ProductManager('productos.json')
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+    res.send(`
+    <h1 style="color: blue; text-align: center">Bienvenidos a ProductManager</h1>
+    `)
+})
+
+app.get('/products', async (req, res) => {
+    const { limit } = req.query
+    const products = await productManager.getProducts()
+
+    let filtrados = []
+
+    if(limit) {
+        if(limit > products.length){
+            res.send(products)
+        } else {
+            for(let i = 0; i < limit; i++){
+                filtrados.push(products[i])
+            }
+    
+            res.send(filtrados)
+        }
+    }
+    else{
+        res.send(products)
+    }
+
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const products = await productManager.getProducts()
+
+    const productId = products.find(prod => prod.id == id)
+
+    if(!productId){
+        res.send({Error: `El producto con el ID: ${id} es inexistente. Pruebe ingresando otro ID`})
+    }
+    else {
+        res.send(productId)
+    }
+
+})
+
+
+const port = 8080
+app.listen(port, () => {
+    console.log(`Servidor leyendose desde http://localhost:${port}`)
+})
